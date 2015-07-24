47b83f407244f10f09e6164e3b7e91fbc45adede
Lucas Oliveira <lucascs.oliveira@gmail.com>
2015-07-24 18:13:11 -0300

    Remoção de imports não utilizados

--- src/main/java/so/coutinho/lucas/graph/methods/search/DepthFirstSearch.java
+++ src/main/java/so/coutinho/lucas/graph/methods/search/DepthFirstSearch.java
@@ -1,6 +1,5 @@
 package so.coutinho.lucas.graph.methods.search;
 
-import com.sun.java.accessibility.util.EventID;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -16,10 +15,10 @@ import so.coutinho.lucas.graph.Vertex;
 @Getter
 @Setter
 public class DepthFirstSearch extends GraphSearch {
-    
+
     private Integer[][] time;
     private List<Vertex> topologicalSorting;
-    
+
     @Override
     public void walkingOnTheGraph(Graph graph) {
         initializeAtributes(graph.getNumberOfVertices());
@@ -29,10 +28,10 @@ public class DepthFirstSearch extends GraphSearch {
                 visitDFS(graph, index, time);
             }
         }
-        
+
         Collections.reverse(topologicalSorting);
     }
-    
+
     private int visitDFS(Graph graph, int index, int time) {
         List<Integer> indexAdjacents = new ArrayList();
         super.getColor()[index] = 'C';
@@ -63,20 +62,20 @@ public class DepthFirstSearch extends GraphSearch {
                 time = visitDFS(graph, indexAdj, ++time);
             }
         }
-        
+
         super.getColor()[index] = 'P';
         this.time[index][1] = ++time;
         topologicalSorting.add(graph.getVertex(index));
-        
+
         return time;
     }
-    
+
     @Override
     protected void initializeAtributes(Integer numberOfVertices) {
         super.initializeAtributes(numberOfVertices);
-        
+
         time = new Integer[numberOfVertices][2];
         topologicalSorting = new ArrayList<>();
     }
-    
+
 }
--- src/main/java/so/coutinho/lucas/graph/methods/shortestpath/DirectedAcyclicGraph.java
+++ src/main/java/so/coutinho/lucas/graph/methods/shortestpath/DirectedAcyclicGraph.java
@@ -1,10 +1,7 @@
 package so.coutinho.lucas.graph.methods.shortestpath;
 
-import java.util.List;
-import so.coutinho.lucas.graph.Edge;
 import so.coutinho.lucas.graph.Graph;
 import so.coutinho.lucas.graph.Vertex;
-import so.coutinho.lucas.graph.methods.search.BreadthFirstSearch;
 import so.coutinho.lucas.graph.methods.search.DepthFirstSearch;
 
 /**
